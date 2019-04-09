b78955c92b7153eb168b69872dbb99c3087170f3
Stefano Magistri <smagistri@iedigital.com>
2019-04-09 12:38:34 +0100

    Add project files.

---
+++ IeDigitalWebFormTest/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IeDigitalWebFormTest.Startup))]
+namespace IeDigitalWebFormTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
